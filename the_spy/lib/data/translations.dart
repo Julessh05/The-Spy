7ee8cc61660218c5404ca37b776e7592032e01ac
Julian Schumacher <professional@julianschumacher.dev>
2024-05-06 17:27:02 +0200

    Add words and translations

--- the_spy/lib/data/translations.dart
+++ the_spy/lib/data/translations.dart
@@ -18,13 +18,120 @@ final Map<String, Map<Locale, String>> _translations = {
   },
   "Start": {
     TranslationLocales.german: "Start",
-  }
+  },
 };
+
+final Map<String, Map<Locale, String>> _wordtranslations = {
+  // Home Words
+  "Bedroom": {
+    TranslationLocales.german: "Schlafzimmer",
+  },
+  "Living Room": {
+    TranslationLocales.german: "Wohnzimmer",
+  },
+  "Kitchen": {
+    TranslationLocales.german: "Küche",
+  },
+  "Garage": {
+    TranslationLocales.german: "Garage",
+  },
+
+  // Country words
+  "Germany": {
+    TranslationLocales.german: "Deutschland",
+  },
+  "United States of America": {
+    TranslationLocales.german: "Vereinigte Staaten von Amerika",
+  },
+  "Great Britain": {
+    TranslationLocales.german: "Großbritannien",
+  },
+  "Australia": {
+    TranslationLocales.german: "Australien",
+  },
+  "Switzerland": {
+    TranslationLocales.german: "Schweiz",
+  },
+  "China": {
+    TranslationLocales.german: "China",
+  },
+  "Austria": {
+    TranslationLocales.german: "Österreich",
+  },
+  "France": {
+    TranslationLocales.german: "Frankreich",
+  },
+  // Places words
+  "Park": {
+    TranslationLocales.german: "Park",
+  },
+  "Mall": {
+    TranslationLocales.german: "Einkaufscenter",
+  },
+  "Restaurant": {
+    TranslationLocales.german: "Restaurant",
+  },
+
+  // Food words
+  "Burger": {
+    TranslationLocales.german: "Burger",
+  },
+  "Bread": {
+    TranslationLocales.german: "Brot",
+  },
+  "Pizza": {
+    TranslationLocales.german: "Pizza",
+  },
+
+  // Vehicle words
+  "Car": {
+    TranslationLocales.german: "Auto",
+  },
+  "Bus": {
+    TranslationLocales.german: "Bus",
+  },
+  // TODO: fahrrad oder motorrad?
+  "Bike": {
+    TranslationLocales.german: "Fahrrad",
+  },
+  "Truck": {
+    TranslationLocales.german: "Laster",
+  },
+
+  // animal words
+  "Horse": {
+    TranslationLocales.german: "Pferd",
+  },
+  "Dog": {
+    TranslationLocales.german: "Hund",
+  },
+  "Cat": {
+    TranslationLocales.german: "Katze",
+  },
+  "Fish": {
+    TranslationLocales.german: "Fisch",
+  },
+  "Mouse": {
+    TranslationLocales.german: "Maus",
+  },
+
+  // technical words
+  "Computer": {
+    TranslationLocales.german: "Computer",
+  },
+  "Camera": {
+    TranslationLocales.german: "Kamera",
+  },
+  "Keyboard": {
+    TranslationLocales.german: "Tastatur",
+  },
+}
 
 /// Getter for the Translations.
 /// Adds the Standard Translations to the
 /// Custom Translations.
 Map<String, Map<Locale, String>> get translations {
   _translations.addAll(StandardTranslations.all);
+  _translations.addAll(_wordtranslations);
   return _translations;
 }
